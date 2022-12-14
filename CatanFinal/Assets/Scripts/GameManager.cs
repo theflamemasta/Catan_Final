9120599cf279a98b5287dfd6b046cc3d0104e1d8
theflamemasta <antonio.dominquez@gmail.com>
2022-12-14 15:52:41 -0500

    Button for Cards Functionality
    Added functionality for resource cards to be added to the hand.

--- CatanFinal/Assets/Scripts/GameManager.cs
+++ CatanFinal/Assets/Scripts/GameManager.cs
@@ -6,6 +6,11 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public List<Card> deck = new List<Card>();
+    public List<Card> brickDeck = new List<Card>();
+    public List<Card> sheepDeck = new List<Card>();
+    public List<Card> woodDeck = new List<Card>();
+    public List<Card> wheatDeck = new List<Card>();
+    public List<Card> oreDeck = new List<Card>();
     public Transform[] cardSlots;
     public bool[] availableCardSlots;
     public Text deckSizeText;
@@ -24,6 +29,101 @@ public class GameManager : MonoBehaviour
                     randCard.transform.position = cardSlots[i].position;
                     availableCardSlots[i] = false;
                     deck.Remove(randCard);
+                    return;
+                }
+            }
+        }
+    }
+    public void BrickDrawCard()
+    {
+        if (brickDeck.Count >= 1)
+        {
+            Card randCard = brickDeck[Random.Range(0, deck.Count)];
+
+            for (int i = 0; i < availableCardSlots.Length; i++)
+            {
+                if (availableCardSlots[i] == true)
+                {
+                    randCard.gameObject.SetActive(true);
+                    randCard.transform.position = cardSlots[i].position;
+                    availableCardSlots[i] = false;
+                    brickDeck.Remove(randCard);
+                    return;
+                }
+            }
+        }
+    }
+    public void SheepDrawCard()
+    {
+        if (sheepDeck.Count >= 1)
+        {
+            Card randCard = sheepDeck[Random.Range(0, deck.Count)];
+
+            for (int i = 0; i < availableCardSlots.Length; i++)
+            {
+                if (availableCardSlots[i] == true)
+                {
+                    randCard.gameObject.SetActive(true);
+                    randCard.transform.position = cardSlots[i].position;
+                    availableCardSlots[i] = false;
+                    sheepDeck.Remove(randCard);
+                    return;
+                }
+            }
+        }
+    }
+    public void WoodDrawCard()
+    {
+        if (woodDeck.Count >= 1)
+        {
+            Card randCard = woodDeck[Random.Range(0, deck.Count)];
+
+            for (int i = 0; i < availableCardSlots.Length; i++)
+            {
+                if (availableCardSlots[i] == true)
+                {
+                    randCard.gameObject.SetActive(true);
+                    randCard.transform.position = cardSlots[i].position;
+                    availableCardSlots[i] = false;
+                    woodDeck.Remove(randCard);
+                    return;
+                }
+            }
+        }
+    }
+    public void WheatDrawCard()
+    {
+        if (wheatDeck.Count >= 1)
+        {
+            Card randCard = wheatDeck[Random.Range(0, deck.Count)];
+
+            for (int i = 0; i < availableCardSlots.Length; i++)
+            {
+                if (availableCardSlots[i] == true)
+                {
+                    randCard.gameObject.SetActive(true);
+                    randCard.transform.position = cardSlots[i].position;
+                    availableCardSlots[i] = false;
+                    wheatDeck.Remove(randCard);
+                    return;
+                }
+            }
+        }
+    }
+    public void OreDrawCard()
+    {
+        if (oreDeck.Count >= 1)
+        {
+            Card randCard = oreDeck[Random.Range(0, deck.Count)];
+
+            for (int i = 0; i < availableCardSlots.Length; i++)
+            {
+                if (availableCardSlots[i] == true)
+                {
+                    randCard.gameObject.SetActive(true);
+                    randCard.transform.position = cardSlots[i].position;
+                    availableCardSlots[i] = false;
+                    oreDeck.Remove(randCard);
                     return;
                 }
             }
